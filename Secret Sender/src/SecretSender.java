e6e4c8e6fdf137f9561ef75fdc5edac2a318578a
francy111 <biribo.francesco@studenti-ittfedifermi.edu.it>
2021-10-29 12:54:09 +0200

    Create classi Secret Sender e Secret Inbox

---
+++ Secret Sender/src/SecretSender.java
@@ -0,0 +1,13 @@
+/**
+ * Classe secret sender
+ * @author francy111
+ * Conterrà le funzionalità del secret sender
+ * 
+ * Inserire codice agente
+ * Inserire IP-Porta secret inbox
+ * Digitare il messaggio da inviare
+ * Selezionare la modalità di cifratura
+ */
+public class SecretSender {
+
+}
