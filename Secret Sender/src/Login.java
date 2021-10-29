00e39d414ee5f4acc6b3895aeafe882c8326f00a
francy111 <biribo.francesco@studenti-ittfedifermi.edu.it>
2021-10-29 20:19:17 +0200

    Aggiunta interfaggia di login e del Secret Sender

---
+++ Secret Sender/src/Login.java
@@ -0,0 +1,126 @@
+/**
+ * Classe Login (Finestra grafica)
+ * @author francy111
+ * @version 1.0
+ * conterr√† ogni elemento presente
+ * nell'interfaccia grafica
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+
+public class Login extends JFrame implements ActionListener{
+	
+	/**
+	 * UID di versione seriale
+	 */
+	private static final long serialVersionUID = 5763669514272369781L;
+
+	/*
+	 * Attributi
+	 */
+	
+	/**
+	 * Etichetta contenente la scritta "Codice agente"
+	 */
+	private JLabel codice;
+	
+	/**
+	 * Campo di testo in cui si dovr‡ inserire il codice dell'agente
+	 */
+	private JTextPane codiceAgente;
+	
+	/**
+	 * Pulsante tramite il quale si conferma il codice, effettuando l'accesso all'applicazione
+	 */
+	private JButton confermaCodice;
+	
+	/**
+	 * Costruttore default
+	 * crea una finestra che fa identificare l'agente tramite il proprio codice.
+	 */
+	public Login() {
+		super("Secret Sender");
+		setBounds(100, 100, 400, 150);
+				
+		codice = new JLabel("Codice agente: ");
+		
+		/* La text pane accetta un massimo di 4
+		 * caratteri (il codice dell'agente sono solo 4 numeri).
+		 * Se si prova a digitare qualcosa dopo 4 caratteri non verr‡ aggiunto niente
+		 * alla stringa gi‡ esistente.
+		 * 
+		 * Per realizzare questo si esegue l'override di insertString, per
+		 * impedire di far inserire caratteri dopo aver raggiunto lunghezza 4.
+		 */
+		codiceAgente = new JTextPane(
+				new DefaultStyledDocument() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void insertString(int offs, String str, AttributeSet a) {
+						if((getLength() + str.length()) <= 4)
+							try {
+								super.insertString(offs, str, a);
+							} catch (BadLocationException e) {
+								e.printStackTrace();
+							}
+					}
+				}
+		);
+		codiceAgente.setPreferredSize(new Dimension(75, 22));
+		codiceAgente.setBorder(LineBorder.createBlackLineBorder());
+		
+		confermaCodice = new JButton("Conferma codice");
+		confermaCodice.addActionListener(this);
+		
+		setLayout(new BorderLayout());
+		
+		JPanel p = new JPanel(), p0 = new JPanel(), p1 = new JPanel();
+		
+		p.add(new JLabel("Per accedere a Secret Sender inserire il codice agente"));
+		p0.add(codice);
+		p0.add(codiceAgente);
+		p1.add(confermaCodice);
+		
+		/* Aggiungiamo i componenti alla finestra
+		 */
+		add(p, BorderLayout.NORTH);
+		add(p0, BorderLayout.CENTER);
+		add(p1, BorderLayout.SOUTH);
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	/**
+	 * Metodo main, crea la finestra grafica
+	 * @param args Argomenti passati da riga di comando
+	 */
+	public static void main(String[] args) {
+		new Login();
+	}
+	
+	/**
+	 * Metodo utilizzato per gestire gli eventi
+	 * @param e Evento√¨
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(confermaCodice)) {
+			try {
+				if(codiceAgente.getText().length()<4) throw new Exception();
+				int code = Integer.parseInt(codiceAgente.getText());
+				
+				new SecretSender(code);
+				dispose();
+			}catch(Exception exp) {
+				JOptionPane.showMessageDialog(null, "Il codice deve contenere solo cifre da 0 a 9", "Errore", 0);
+			}
+		}
+	}
+}
--- Secret Sender/src/SecretSender.java
+++ Secret Sender/src/SecretSender.java
@@ -1,6 +1,13 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 /**
  * Classe secret sender
  * @author francy111
+ * @version 1.0
  * Conterrà le funzionalità del secret sender
  * 
  * Inserire codice agente
@@ -8,6 +15,111 @@
  * Digitare il messaggio da inviare
  * Selezionare la modalità di cifratura
  */
-public class SecretSender {
-
+public class SecretSender extends JFrame{
+	private static final long serialVersionUID = 1L;
+			
+	/**
+	 * Attributi 
+	 */
+	private int code;
+	private ArrayList<Pair> inboxes;
+	private JButton impostazioni;
+	private JTextField ricerca;
+	private JTextField infoChat;
+	private JPanel listaChat;
+	private JTextArea cronologia;
+	private JTextField zonaMessaggio;
+	private JButton inviaMessaggio;
+	
+	private JRadioButton cesare, vigenere;
+	
+	private JButton chiudiApplicazione;
+	private JButton logout;
+	
+	/**
+	 * Costruttore default
+	 * @param code Codice dell'agente che ha eseguito l'accesso
+	 */
+	public SecretSender(int code) {
+		super("Secret Sender");
+		setBounds(100, 100, 945, 550);
+		setLayout(null);
+		setResizable(false);
+		setBackground(Color.white);
+		
+		/* Inizializzazione attributi
+		 */
+		this.code = code;
+		inboxes = new ArrayList<>();
+		
+		impostazioni = new JButton("TK");
+		impostazioni.setBounds(0,0,55,55);
+		
+		ricerca = new JTextField();
+		ricerca.setBounds(55,0,217,56);
+		ricerca.setText("Ricerca");
+		
+		infoChat = new JTextField();
+		infoChat.setBounds(270,0,660,56);
+		infoChat.setText("Placeholder - ip - porta");
+		infoChat.setVisible(false);
+		
+		listaChat = new JPanel();
+		listaChat.setBorder(LineBorder.createBlackLineBorder());
+		listaChat.setBounds(0,55,271,456);
+		listaChat.setLayout(new BoxLayout(listaChat,BoxLayout.Y_AXIS));
+		
+		cronologia = new JTextArea();
+		cronologia.setBorder(LineBorder.createBlackLineBorder());
+		cronologia.setBounds(270,55,660,400);
+		cronologia.setVisible(false);
+		
+		zonaMessaggio = new JTextField();
+		zonaMessaggio.setBounds(270,455,601,57);
+		zonaMessaggio.setText("Scrivi un messaggio...");
+		zonaMessaggio.setVisible(false);
+		
+		inviaMessaggio = new JButton("TK");
+		inviaMessaggio.setBounds(870,455, 60, 55);
+		inviaMessaggio.setVisible(false);
+		
+		cesare = new JRadioButton();
+		vigenere = new JRadioButton();
+		ButtonGroup r = new ButtonGroup();
+		r.add(cesare);
+		r.add(vigenere);
+		
+		chiudiApplicazione = new JButton("Exit");
+		chiudiApplicazione.setVisible(false);
+		
+		logout = new JButton("Esegui logout");
+		logout.setVisible(false);
+		
+		
+		/* Aggiunta dei componenti alla
+		 * finestra
+		 */
+		add(impostazioni);
+		add(ricerca);
+		add(infoChat);
+		add(listaChat);
+		add(cronologia);
+		add(zonaMessaggio);
+		add(inviaMessaggio);
+		add(chiudiApplicazione);
+		add(logout);
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	/**
+	 * Classe coppia
+	 * @author francy111
+	 * Usata per accoppiare IP e Porta di un Secret Inbox
+	 */
+	private class Pair{
+		public int port;
+		public String IP;
+	}
 }
